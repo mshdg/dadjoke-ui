937d493b18651b3d318b21fa44054e692af0acf0
Jason Grant <jgrant1975@gmail.com>
2020-02-15 18:29:08 -0500

    Added a baisc health check

--- app/main.py
+++ app/main.py
@@ -42,5 +42,10 @@ def favicon():
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
+@app.route('/health')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'health.html')
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
